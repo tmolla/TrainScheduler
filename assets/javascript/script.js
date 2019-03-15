3cf9614781421aad714b7456cd3b1e9f8e2ca1ac
Telahoun Molla <telahounmolla@gmail.com>
2019-03-14 17:25:02 -0700

    Added delete and update of records

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -10,6 +10,9 @@
   firebase.initializeApp(config);
 
 var database = firebase.database();
+var timerIntervalID = "";
+var counter = 0
+var dataArray = [];
 
 function calculateTimeRemainder(firstTime, frequency){
     // First Time (pushed back 1 year to make sure it comes before current time)
@@ -25,8 +28,30 @@ function minutesAway(firstTime, frequency) {
 function nextArrivalTime(firstTime, frequency){
     return(moment().add(minutesAway(firstTime, frequency), "minutes").format("hh:mm"));
 }
+function displayRecord(sp){
+    var row = {}
+    row = {key:sp.key, value:sp.val()};
 
-$(".btn-primary").on("click", function (event) {
+    //push each record into an array so we can use it with setInterval
+    dataArray.push(row);
+    var tfrequency = sp.val().frequency;
+    var tFirstTime = sp.val().firstTime; 
+    var row = $("<tr>");
+    var cols = '<td><span class="fas fa-pencil-alt float-left" data-key="' + sp.key + '"></span></td>' +
+    '<td><span class="fas fa-trash float-left" data-key="' + sp.key + '"></span></td>' +
+    '<td>'+ sp.val().name + '</td>' +
+    '<td>'+ sp.val().destination + '</td>' +
+    '<td>'+ tfrequency + '</td>' +
+    '<td>'+ nextArrivalTime(tFirstTime, tfrequency) + '</td>' +
+    '<td>'+ minutesAway(tFirstTime, tfrequency) + '</td>'; 
+    
+    row.addClass(sp.key);
+    row.append(cols);
+    $("table tbody").append(row) 
+    //console.log(dataArray);
+}
+
+$(".addData").on("click", function (event) {
     event.preventDefault();
 
     var newTrain = {
@@ -39,10 +64,51 @@ $(".btn-primary").on("click", function (event) {
     database.ref().push(newTrain);
 
     $(".add").hide();
+    $(".update").hide();
     $(".schedule").show();
 });
 
+$(document).on("click", ".fa-trash", function(){
+    database.ref($(this).attr('data-key')).remove()
+    .then(function() {
+        console.log("Remove succeeded.")
+    })
+    .catch(function(error) {
+        console.log("Remove failed: " + error.message)
+    });
+});
 
+$(document).on("click", ".fa-pencil-alt", function(){
+    //get record
+    alert("you touched the pencil")
+    $(".schedule").hide();
+    database.ref($(this).attr('data-key'))
+    .once("value").then(function(sp) {
+        $("#formGroupUpdateTrainName").val(sp.val().name);
+        $("#formGroupUpdateDestination").val(sp.val().destination);
+        $("#formGroupUpdateFirstTrainTime").val(sp.val().firstTime);
+        $("#formGroupUpdateFrequency").val(sp.val().frequency);
+        //console.log(sp.val().name);
+        //console.log(sp.val().destination);
+      });
+    $(".updateData").attr("data-key", $(this).attr('data-key'));
+    $(".update").show();
+});
+
+$(".updateData").on("click", function(){
+    var updatedSchedule = {
+        name: $("#formGroupUpdateTrainName").val().trim(),
+        destination: $("#formGroupUpdateDestination").val().trim(),
+        firstTime: $("#formGroupUpdateFirstTrainTime").val().trim(),
+        frequency: $("#formGroupUpdateFrequency").val().trim(),
+        dateUpdated: firebase.database.ServerValue.TIMESTAMP
+    };
+    console.log(updatedSchedule);
+console.log($(this).attr('data-key'))
+   database.ref().child($(this).attr('data-key')).update(updatedSchedule)
+    $(".update").hide();
+    $(".schedule").show();
+});
 $(".addTrain").on("click", function(){
     $(".add").show();
     $(".schedule").hide();
@@ -50,21 +116,35 @@ $(".addTrain").on("click", function(){
 
 
 database.ref().on("child_added", function(sp) {
-    var tfrequency = sp.val().frequency;
-    var tFirstTime = sp.val().firstTime; 
-    var row = '<tr><td><span class="fas fa-pencil-alt float-left"></span></td>' +
-    '<td><span class="fas fa-trash float-left"></span></td>' +
-    '<td>'+ sp.val().name + '</td>' +
-    '<td>'+ sp.val().destination + '</td>' +
-    '<td>'+ tfrequency + '</td>' +
-    '<td>'+ nextArrivalTime(tFirstTime, tfrequency) + '</td>' +
-    '<td>'+ minutesAway(tFirstTime, tfrequency) + '</td></tr>'; 
-    $("table tbody").append(row) 
+    //console.log(sp.key);
+    displayRecord(sp);
 }, function(error) {
     console.log("Error: " + error.code);
 });
 
+database.ref().on("child_removed", function(sp){
+    $('.'+sp.key).remove()
+});
+
+database.ref().on("child_changed", function(sp){
+    //alert("Hey child changed " + sp.key)
+});
+
 $(document).ready(function(){
     $(".add").hide();
+    $(".update").hide();
     $(".schedule").show();
+    //timerIntervalID = setInterval(updateTable, 10000);
+    //alert("In ready")
+});
+
+function updateTable(){
+    counter++;
+    alert("I was called");
+    if (counter >= 2){
+        clearInterval(timerIntervalID);
+    }
+}
+$(document).on('click', 'tr', function(){
+    //alert("you clicked on a row");
 });
